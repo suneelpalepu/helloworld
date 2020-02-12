d7cf512da9dfa12da05b99eba84b1f09c797dc6f
Suneel <suneel.1082@gmail.com>
2020-02-12 09:59:12 +0530

    first commit

---
+++ src/main/scala/test/HelloWorld.scala
@@ -0,0 +1,9 @@
+package test
+/*
+this is for test purpose only
+ */
+object HelloWorld {
+   def main(args: Array[String]):Unit = {
+     println("Let's Do BigData Analytics") //print this line
+   }
+}
